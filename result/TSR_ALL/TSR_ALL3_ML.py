9c9b6ffb9f2f28cb8487e39441feb968983c5458
sn987532159 <rmhiche@ucl.ac.uk>
2021-06-08 21:22:08 +0800

    updated

--- result/TSR_ALL/TSR_ALL3_ML.py
+++ result/TSR_ALL/TSR_ALL3_ML.py
@@ -2,33 +2,35 @@ import pandas as pd
 import os
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score
+#from sklearn.metrics import roc_auc_score
+from sklearn.metrics import auc, roc_curve
 from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from xgboost import XGBClassifier
 from sklearn.metrics import confusion_matrix
 
 # Import datasets
+
 csv_path = os.path.join("..", "..", "data","LINKED_DATA", "TSR_ALL", "TSR_ALL3_MICE5.csv")
 tsr_3_imp_mean = pd.read_csv(csv_path)
 tsr_3_imp_mean.shape
 
 # Convert the multiple feature and outcome into binary ones
+
 mRS3 = tsr_3_imp_mean.mrs_tx_3
 mRS3[(mRS3 == 0) | (mRS3 == 1) | (mRS3 == 2)] = 1 #GOOD
 mRS3[(mRS3 == 3) | (mRS3 == 4) | (mRS3 == 5) | (mRS3 == 6) | (mRS3 == 9)] = 0 #BAD
-
 # mRS1 = tsr_3_imp_mean.mrs_tx_1
 # mRS1[(mRS1 == 0) | (mRS1 == 1) | (mRS1 == 2)] = 1 #GOOD
 # mRS1[(mRS1 == 3) | (mRS1 == 4) | (mRS1 == 5) | (mRS1 == 6) | (mRS1 == 9)] = 0 #BAD
-
 # discharged = tsr_3_imp_mean.discharged_mrs
 # discharged[(discharged == 0) | (discharged == 1) | (discharged == 2)] = 1 #GOOD
 # discharged[(discharged == 3) | (discharged == 4) | (discharged == 5) | (discharged == 6) | (discharged == 9)] = 0 #BAD
 
-# Machine learning
-## Group all features and the outcome
+# Group all features and the outcome
+
 nominal_features = ["edu_id", "pro_id", "opc_id", "toast_id", "offdt_id", "gender_tx", "hd_id", "pcva_id",
                     "pcvaci_id", "pcvach_id", "po_id", "ur_id", "sm_id", "ptia_id", "hc_id", "hcht_id",
                     "hchc_id", "ht_id", "dm_id", "pad_id", "al_id", "ca_id", "fahiid_parents_1",
@@ -69,9 +71,12 @@ continuous = ["height_nm", "weight_nm", "sbp_nm", "dbp_nm", "bt_nm", "hr_nm", "r
 labels = ["mrs_tx_3", "mrs_tx_1"]
 
 # Machine Learning
-# Preprocess input data (GOOD when Discharge)
+
+## Preprocess input data (GOOD when Discharge)
+
 ## discharged mRS = GOOD (tsr_3_imp_mean.discharged_mrs == 1)
 mrs_dis1 = tsr_3_imp_mean[(tsr_3_imp_mean.discharged_mrs == 1) | (tsr_3_imp_mean.discharged_mrs == 0) | (tsr_3_imp_mean.discharged_mrs == 2)]
+##########mrs_dis1["mrs_tx_3"][mrs_dis1["mrs_tx_3"] == 9] = 7
 
 ## input dataset
 tsr_3G_input = mrs_dis1.drop(["mrs_tx_1", "mrs_tx_3"], axis=1)
@@ -127,7 +132,6 @@ G_X_test = G_X_test.drop(nominal_features, axis=1)
 G_X_test = np.array(G_X_test.values)
 
 ## Oversampling
-
 from collections import Counter
 from imblearn.over_sampling import SMOTE
 print('Original dataset shape %s' % Counter(G_y_train))
@@ -136,7 +140,6 @@ G_X_train_smote, G_y_train_smote = smote.fit_resample(G_X_train, G_y_train)
 print('Resampled dataset shape %s' % Counter(G_y_train_smote))
 
 ## Undersampling
-
 # Method 1 - RandomUnderSampler
 from collections import Counter
 from imblearn.under_sampling import RandomUnderSampler
@@ -154,42 +157,227 @@ G_X_train_tl, G_y_train_tl = tl.fit_resample(G_X_train, G_y_train)
 print('Resampled dataset shape %s' % Counter(G_y_train_tl))
 
 ## Algorithms
+#____________________________________________________________
+### try decision tree pruning
+from sklearn.tree import DecisionTreeClassifier
+path=DecisionTreeClassifier(random_state=1).cost_complexity_pruning_path(G_X_train_tl, G_y_train_tl)
+ccp_alphas, impurities = path.ccp_alphas, path.impurities
 
-rf = RandomForestClassifier(random_state=19, class_weight={0: 1, 1: 24})  # when using tl
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+ax.plot(ccp_alphas[:-1], impurities[:-1], marker='o', drawstyle="steps-post")
+ax.set_xlabel("effective alpha")
+ax.set_ylabel("total impurity of leaves")
+ax.set_title("Total Impurity vs effective alpha for training set")
+plt.show()
+
+clfs = []
+for ccp_alpha in ccp_alphas:
+    clf = DecisionTreeClassifier(random_state=1, ccp_alpha=ccp_alpha)
+    clf.fit(G_X_train_tl, G_y_train_tl)
+    clfs.append(clf)
+
+print("Number of nodes in the last tree is: {} with ccp_alpha: {} and a depth of: {}".format(clfs[-1].tree_.node_count, ccp_alphas[-1],clfs[-1].tree_.max_depth))
+
+clfs = clfs[:-1]
+ccp_alphas = ccp_alphas[:-1]
+
+node_counts = [clf.tree_.node_count for clf in clfs]
+depth = [clf.tree_.max_depth for clf in clfs]
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(ccp_alphas, node_counts, marker='o', drawstyle="steps-post")
+ax[0].set_xlabel("alpha")
+ax[0].set_ylabel("number of nodes")
+ax[0].set_title("Number of nodes vs alpha")
+ax[1].plot(ccp_alphas, depth, marker='o', drawstyle="steps-post")
+ax[1].set_xlabel("alpha")
+ax[1].set_ylabel("depth of tree")
+ax[1].set_title("Depth vs alpha")
+#fig.tight_layout()
+plt.show()
+
+train_scores = [clf.score(G_X_train_tl, G_y_train_tl) for clf in clfs]
+test_scores = [clf.score(G_X_test, G_y_test) for clf in clfs]
+
+fig, ax = plt.subplots()
+ax.set_xlabel("alpha")
+ax.set_ylabel("accuracy")
+ax.set_title("Accuracy vs alpha for training and testing sets")
+ax.plot(ccp_alphas, train_scores, marker='o', label="train",
+        drawstyle="steps-post")
+ax.plot(ccp_alphas, test_scores, marker='o', label="test",
+        drawstyle="steps-post")
+ax.legend()
+plt.show()
+
+index_best_model = np.argmax(test_scores)
+best_model = clfs[index_best_model]
+print('Training accuracy of best model: ',best_model.score(G_X_train_tl, G_y_train_tl))
+print('Test accuracy of best model: ',best_model.score(G_X_test, G_y_test))
+
+best_model.fit(G_X_train_tl, G_y_train_tl)
+print("AUC of training set:", roc_auc_score(G_y_train_tl, best_model.predict(G_X_train_tl)))
+G_y_predicted = best_model.predict(G_X_test)
+print("AUC of testing set:", roc_auc_score(G_y_test, G_y_predicted))
+confusion_matrix(G_y_test, G_y_predicted)
+#____________________________________________________________
+
+### base rf
+rf = RandomForestClassifier(random_state=19, class_weight={0: 1, 1: 25}) # when using tl
+# rf = RandomForestClassifier(random_state=19, class_weight={0: 1, 1: 25}, ccp_alpha = 0.000009) # when using tl
 # rf = RandomForestClassifier(random_state=19)
 rf.fit(G_X_train_tl, G_y_train_tl)
-print("AUC of training set:", roc_auc_score(G_y_train_tl, rf.predict(G_X_train_tl)))
-G_y_predicted = rf.predict(G_X_test)
-print("AUC of testing set:", roc_auc_score(G_y_test, G_y_predicted))
-confusion_matrix(G_y_test, G_y_predicted)
+G_y_train_pred = rf.predict_proba(G_X_train_tl)
+fpr, tpr, thresholds = roc_curve(G_y_train_tl, G_y_train_pred[:, 1])
+G_train_auroc = auc(fpr, tpr)
+print('AUC of training set:', G_train_auroc)
+G_y_test_pred = rf.predict_proba(G_X_test)
+fpr, tpr, thresholds = roc_curve(G_y_test, G_y_test_pred[:, 1])
+G_test_auroc = auc(fpr, tpr)
+print('AUC of testing set:', G_test_auroc)
 
-svc = SVC(random_state=19, class_weight={0: 24, 1: 1})  # when using tl
-# svc = SVC(random_state=19)
+# confusion_matrix(G_y_test, G_y_predicted)
+
+### base et
+et = ExtraTreesClassifier(random_state=19, class_weight={0: 1, 1: 25}) # when using tl
+# et = ExtraTreesClassifier(random_state=19, class_weight={0: 1, 1: 25}, ccp_alpha = 0.000009) # when using tl
+# et = ExtraTreesClassifier(random_state=19)
+et.fit(G_X_train_tl, G_y_train_tl)
+G_y_train_pred = et.predict_proba(G_X_train_tl)
+fpr, tpr, thresholds = roc_curve(G_y_train_tl, G_y_train_pred[:, 1])
+G_train_auroc = auc(fpr, tpr)
+print('AUC of training set:', G_train_auroc)
+G_y_test_pred = et.predict_proba(G_X_test)
+fpr, tpr, thresholds = roc_curve(G_y_test, G_y_test_pred[:, 1])
+G_test_auroc = auc(fpr, tpr)
+print('AUC of testing set:', G_test_auroc)
+
+# confusion_matrix(G_y_test, G_y_predicted)
+
+### base svc
+svc = SVC(random_state=19, probability = True, class_weight={0: 1, 1: 25})  # when using tl
+# svc = SVC(random_state=19, probability = True)
 svc.fit(G_X_train_tl, G_y_train_tl)
-print("AUC of training set:", roc_auc_score(G_y_train_tl, svc.predict(G_X_train_tl)))
-G_y_predicted = svc.predict(G_X_test)
-print("AUC of testing set:", roc_auc_score(G_y_test, G_y_predicted))
-confusion_matrix(G_y_test, G_y_predicted)
+G_y_train_pred = svc.predict_proba(G_X_train_tl)
+fpr, tpr, thresholds = roc_curve(G_y_train_tl, G_y_train_pred[:, 1])
+G_train_auroc = auc(fpr, tpr)
+print('AUC of training set:', G_train_auroc)
+G_y_test_pred = svc.predict_proba(G_X_test)
+fpr, tpr, thresholds = roc_curve(G_y_test, G_y_test_pred[:, 1])
+G_test_auroc = auc(fpr, tpr)
+print('AUC of testing set:', G_test_auroc)
 
-lsvc = LinearSVC(random_state=19, class_weight={0: 24, 1: 1})  # when using tl
-# lsvc = LinearSVC(random_state=19)
+# confusion_matrix(G_y_test, G_y_predicted)
+
+### lsvc
+lsvc = LinearSVC(random_state=19, dual=False, class_weight={0: 1, 1: 25})  # when using tl
+# lsvc = LinearSVC(dual=False, random_state=19)
 lsvc.fit(G_X_train_tl, G_y_train_tl)
-print("AUC of training set:", roc_auc_score(G_y_train_tl, lsvc.predict(G_X_train_tl)))
-G_y_predicted = lsvc.predict(G_X_test)
-print("AUC of testing set:", roc_auc_score(G_y_test, G_y_predicted))
-confusion_matrix(G_y_test, G_y_predicted)
+G_y_train_pred = lsvc._predict_proba_lr(G_X_train_tl)
+fpr, tpr, thresholds = roc_curve(G_y_train_tl, G_y_train_pred[:, 1])
+G_train_auroc = auc(fpr, tpr)
+print('AUC of training set:', G_train_auroc)
+G_y_test_pred = lsvc._predict_proba_lr(G_X_test)
+fpr, tpr, thresholds = roc_curve(G_y_test, G_y_test_pred[:, 1])
+G_test_auroc = auc(fpr, tpr)
+print('AUC of testing set:', G_test_auroc)
 
+# confusion_matrix(G_y_test, G_y_predicted)
+
+### xgbc
 xgbc = XGBClassifier(random_state=19, use_label_encoder=False, eval_metric="auc",
-                     scale_pos_weight=(1 / 24))  # when using tl
+                     scale_pos_weight=(1 / 25))  # when using tl
 # xgbc = XGBClassifier(random_state=19, use_label_encoder=False, eval_metric = "auc")
 xgbc.fit(G_X_train_tl, G_y_train_tl)
-print("AUC of training set:", roc_auc_score(G_y_train_tl, xgbc.predict(G_X_train_tl)))
-G_y_predicted = xgbc.predict(G_X_test)
-print("AUC of testing set:", roc_auc_score(G_y_test, G_y_predicted))
-confusion_matrix(G_y_test, G_y_predicted)
+G_y_train_pred = xgbc.predict_proba(G_X_train_tl)
+fpr, tpr, thresholds = roc_curve(G_y_train_tl, G_y_train_pred[:, 1])
+G_train_auroc = auc(fpr, tpr)
+print('AUC of training set:', G_train_auroc)
+G_y_test_pred = xgbc.predict_proba(G_X_test)
+fpr, tpr, thresholds = roc_curve(G_y_test, G_y_test_pred[:, 1])
+G_test_auroc = auc(fpr, tpr)
+print('AUC of testing set:', G_test_auroc)
 
+# confusion_matrix(G_y_test, G_y_predicted)
+
+### neural network
 import torch
+import torch.nn.functional as F
 import torch.nn as nn
+from torch.autograd import Variable
+
+class Model(nn.Module):
+    def __init__(self, input_dataset):
+        super(Model, self).__init__()
+        self.layer1 = nn.Linear(input_dataset, 50)
+        self.layer2 = nn.Linear(50, 50)
+        self.layer3 = nn.Linear(50, 2)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = F.softmax(self.layer3(x), dim = 1) # dim = 1 -> compute by rows
+        return x
+
+model = Model(G_X_train_tl.shape[1])
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+loss_fn = nn.CrossEntropyLoss()
+model
+
+import tqdm
+EPOCHS = 100
+G_X_train_tl = Variable(torch.from_numpy(G_X_train_tl)).float()
+G_y_train_tl = Variable(torch.from_numpy(G_y_train_tl)).long()
+G_X_test = Variable(torch.from_numpy(G_X_test)).float()
+G_y_test = Variable(torch.from_numpy(G_y_test)).long()
+
+loss_list = np.zeros((EPOCHS,))
+accuracy_list = np.zeros((EPOCHS,))
+
+for epoch in tqdm.trange(EPOCHS):
+    y_pred = model(G_X_train_tl)
+    loss = loss_fn(y_pred, G_y_train_tl)
+    loss_list[epoch] = loss.item()
+
+    # Zero gradients
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    with torch.no_grad():
+        y_pred = model(G_X_test)
+        correct = (torch.argmax(y_pred, dim=1) == G_y_test).type(torch.FloatTensor)
+        accuracy_list[epoch] = correct.mean()
+
+import matplotlib.pyplot as plt
+fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 6), sharex=True)
+ax1.plot(accuracy_list)
+ax1.set_ylabel("validation accuracy")
+ax2.plot(loss_list)
+ax2.set_ylabel("validation loss")
+ax2.set_xlabel("epochs");
+plt.show()
+
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import OneHotEncoder
+
+plt.figure(figsize=(10, 10))
+plt.plot([0, 1], [0, 1], 'k--')
+
+# One hot encoding
+enc = OneHotEncoder()
+Y_onehot = enc.fit_transform(G_y_test[:, np.newaxis]).toarray()
+
+with torch.no_grad():
+    y_pred = model(G_X_test).numpy()
+    fpr, tpr, threshold = roc_curve(Y_onehot.ravel(), y_pred.ravel())
+
+plt.plot(fpr, tpr, label='AUC = {:.3f}'.format(auc(fpr, tpr)))
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve')
+plt.legend()
+plt.show()
 
 # Anomaly detection
 G_X_train, G_X_test, G_y_train, G_y_test = train_test_split(tsr_3G_input, tsr_3G_output, test_size=0.3, random_state=19)
@@ -252,9 +440,12 @@ prn = round(precision_n_scores(G_y_test, y_test_scores), ndigits=4)
 print(f'{clf} AUC:{auc}, precision @ rank n:{prn}')
 
 # Machine Learning
-# Preprocess input data (BAD when Discharge)
+
+## Preprocess input data (BAD when Discharge)
+
 # discharged mRS = BAD (tsr_3_imp_mean.discharged_mrs == 0)
-mrs_dis0 = tsr_3_imp_mean[tsr_3_imp_mean.discharged_mrs == 0]
+mrs_dis0 = tsr_3_imp_mean[(tsr_3_imp_mean.discharged_mrs != 1) & (tsr_3_imp_mean.discharged_mrs != 0) & (tsr_3_imp_mean.discharged_mrs != 2)]
+##########mrs_dis0["mrs_tx_3"][mrs_dis0["mrs_tx_3"] == 9] = 7
 
 ## input dataset
 tsr_3B_input = mrs_dis0.drop(["mrs_tx_1", "mrs_tx_3"], axis=1)
@@ -312,12 +503,14 @@ B_X_test = B_X_test.drop(nominal_features, axis=1)
 B_X_test = np.array(B_X_test.values)
 
 ## Oversampling
+
 print('Original dataset shape %s' % Counter(B_y_train))
 smote = SMOTE(sampling_strategy='minority', random_state=19)  # oversampling
 B_X_train_smote, B_y_train_smote = smote.fit_resample(B_X_train, B_y_train)
 print('Resampled dataset shape %s' % Counter(B_y_train_smote))
 
 ## Undersampling
+
 # Method 1 - RandomUnderSampler
 print('Original dataset shape %s' % Counter(B_y_train))
 rus = RandomUnderSampler(sampling_strategy='majority', random_state=19)  # oversampling
@@ -333,15 +526,22 @@ B_X_train_tl, B_y_train_tl = tl.fit_resample(B_X_train, B_y_train)
 print('Resampled dataset shape %s' % Counter(B_y_train_tl))
 
 ## Algorithms
+
 ### base rf
-rf = RandomForestClassifier(random_state=19,
-                            class_weight={0: (680 / 680 + 13446), 1: (13446 / 680 + 13446)})  # when using tl
+rf = RandomForestClassifier(random_state=19, class_weight={0: 1, 1: 25}) # when using tl
+# rf = RandomForestClassifier(random_state=19, class_weight={0: 1, 1: 25}, ccp_alpha = 0.000009) # when using tl
 # rf = RandomForestClassifier(random_state=19)
 rf.fit(B_X_train_tl, B_y_train_tl)
-print("AUC of training set:", roc_auc_score(B_y_train_tl, rf.predict(B_X_train_tl)))
-B_y_predicted = rf.predict(B_X_test)
-print("AUC of testing set:", roc_auc_score(B_y_test, B_y_predicted))
-confusion_matrix(B_y_test, B_y_predicted)
+B_y_train_pred = rf.predict_proba(B_X_train_tl)
+fpr, tpr, thresholds = roc_curve(B_y_train_tl, B_y_train_pred[:, 1])
+B_train_auroc = auc(fpr, tpr)
+print('AUC of training set:', B_train_auroc)
+B_y_test_pred = rf.predict_proba(B_X_test)
+fpr, tpr, thresholds = roc_curve(B_y_test, B_y_test_pred[:, 1])
+B_test_auroc = auc(fpr, tpr)
+print('AUC of testing set:', B_test_auroc)
+
+# confusion_matrix(B_y_test, B_y_predicted)
 
 ### tune rf
 from sklearn.model_selection import GridSearchCV
@@ -381,14 +581,36 @@ B_y_predicted = best_rf.predict(B_X_test)
 print("AUC of testing set:", roc_auc_score(B_y_test, B_y_predicted))
 confusion_matrix(B_y_test, B_y_predicted)
 
+### base et
+et = ExtraTreesClassifier(random_state=19, class_weight={0: 1, 1: 25}) # when using tl
+# et = ExtraTreesClassifier(random_state=19, class_weight={0: 1, 1: 25}, ccp_alpha = 0.000009) # when using tl
+# et = ExtraTreesClassifier(random_state=19)
+et.fit(B_X_train_tl, B_y_train_tl)
+B_y_train_pred = et.predict_proba(B_X_train_tl)
+fpr, tpr, thresholds = roc_curve(B_y_train_tl, B_y_train_pred[:, 1])
+B_train_auroc = auc(fpr, tpr)
+print('AUC of training set:', B_train_auroc)
+B_y_test_pred = et.predict_proba(B_X_test)
+fpr, tpr, thresholds = roc_curve(B_y_test, B_y_test_pred[:, 1])
+B_test_auroc = auc(fpr, tpr)
+print('AUC of testing set:', B_test_auroc)
+
+# confusion_matrix(B_y_test, B_y_predicted)
+
 ### base svc
-svc = SVC(random_state=19, class_weight={0: (680 / 680 + 13446), 1: (13446 / 680 + 13446)})  # when using tl
-# svc = SVC(random_state=19)
+svc = SVC(random_state=19, probability = True, class_weight={0: 1, 1: 25})  # when using tl
+# svc = SVC(random_state=19, probability = True)
 svc.fit(B_X_train_tl, B_y_train_tl)
-print("AUC of training set:", roc_auc_score(B_y_train_tl, svc.predict(B_X_train_tl)))
-B_y_predicted = svc.predict(B_X_test)
-print("AUC of testing set:", roc_auc_score(B_y_test, B_y_predicted))
-confusion_matrix(B_y_test, B_y_predicted)
+B_y_train_pred = svc.predict_proba(B_X_train_tl)
+fpr, tpr, thresholds = roc_curve(B_y_train_tl, B_y_train_pred[:, 1])
+B_train_auroc = auc(fpr, tpr)
+print('AUC of training set:', B_train_auroc)
+B_y_test_pred = svc.predict_proba(B_X_test)
+fpr, tpr, thresholds = roc_curve(B_y_test, B_y_test_pred[:, 1])
+B_test_auroc = auc(fpr, tpr)
+print('AUC of testing set:', B_test_auroc)
+
+# confusion_matrix(B_y_test, B_y_predicted)
 
 ### tune svc
 param_range = [0.01, 0.1, 1, 10, 100]
@@ -423,20 +645,32 @@ print("AUC of testing set:", roc_auc_score(B_y_test, B_y_predicted))
 confusion_matrix(B_y_test, B_y_predicted)
 
 ### base lsvc
-lsvc = LinearSVC(random_state=19, class_weight={0: (680 / 680 + 13446), 1: (13446 / 680 + 13446)})  # when using tl
+lsvc = LinearSVC(random_state=19, dual=False, class_weight={0: 1, 1: 25})  # when using tl
 # lsvc = LinearSVC(random_state=19)
 lsvc.fit(B_X_train_tl, B_y_train_tl)
-print("AUC of training set:", roc_auc_score(B_y_train_tl, lsvc.predict(B_X_train_tl)))
-B_y_predicted = lsvc.predict(B_X_test)
-print("AUC of testing set:", roc_auc_score(B_y_test, B_y_predicted))
-confusion_matrix(B_y_test, B_y_predicted)
+B_y_train_pred = lsvc._predict_proba_lr(B_X_train_tl)
+fpr, tpr, thresholds = roc_curve(B_y_train_tl, B_y_train_pred[:, 1])
+B_train_auroc = auc(fpr, tpr)
+print('AUC of training set:', B_train_auroc)
+B_y_test_pred = lsvc._predict_proba_lr(B_X_test)
+fpr, tpr, thresholds = roc_curve(B_y_test, B_y_test_pred[:, 1])
+B_test_auroc = auc(fpr, tpr)
+print('AUC of testing set:', B_test_auroc)
+
+# confusion_matrix(B_y_test, B_y_predicted)
 
 ### base xgbc
 xgbc = XGBClassifier(random_state=19, use_label_encoder=False, eval_metric="auc",
-                     scale_pos_weight=(680 / 13445))  # when using tl
+                     scale_pos_weight=(1 / 25))  # when using tl
 # xgbc = XGBClassifier(random_state=19, use_label_encoder=False, eval_metric = "auc")
 xgbc.fit(B_X_train_tl, B_y_train_tl)
-print("AUC of training set:", roc_auc_score(B_y_train_tl, xgbc.predict(B_X_train_tl)))
-B_y_predicted = xgbc.predict(B_X_test)
-print("AUC of testing set:", roc_auc_score(B_y_test, B_y_predicted))
-confusion_matrix(B_y_test, B_y_predicted)
+B_y_train_pred = xgbc.predict_proba(B_X_train_tl)
+fpr, tpr, thresholds = roc_curve(B_y_train_tl, B_y_train_pred[:, 1])
+B_train_auroc = auc(fpr, tpr)
+print('AUC of training set:', B_train_auroc)
+B_y_test_pred = xgbc.predict_proba(B_X_test)
+fpr, tpr, thresholds = roc_curve(B_y_test, B_y_test_pred[:, 1])
+B_test_auroc = auc(fpr, tpr)
+print('AUC of testing set:', B_test_auroc)
+
+# confusion_matrix(B_y_test, B_y_predicted)
